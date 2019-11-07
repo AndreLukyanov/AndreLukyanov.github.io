5651eabe706e7965b9477e4aa9755d35235f0d42
AndreLukyanov <Lukyanov-Andrey-V@yandex.ru>
2019-11-08 00:00:00 +0300

    Добавление кликов на табы

--- analysis/js/script.js
+++ analysis/js/script.js
@@ -1,7 +1,42 @@
 var source = $('#entry-template').html();
 var template = Handlebars.compile(source);
 
-var currentPage = 10;
-employees.emps.length = currentPage;
+$('.entry-table').html(template(employees.slice(0,10)));
 
-$('.entry-table').html(template(employees));
+
+// var servises = (function (pages) {
+
+// 	var size = 10;
+
+// 	function getData() {
+// 		return {
+// 			function() {
+// 				employees.slice(pages * size + (pages + 1));
+// 				console.log(employees);
+// 			}
+// 		}
+// 	}
+// 	return {
+// 		method: function () {
+// 			getData();
+// 		}
+// 	};
+// })();
+// servises.method();
+
+
+
+
+
+function firstClick() {
+	$('.entry-table').html(template(employees.slice(0,10)));
+}
+
+function secondClick() {
+	$('.entry-table').html(template(employees.slice(10,20)));
+}
+
+function threeClick() {
+	$('.entry-table').html(template(employees.slice(20,30)));
+}
+
