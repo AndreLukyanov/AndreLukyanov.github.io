22a65a567d0106ed4adbfbd1bc6de3420a258608
AndreLukyanov <44945472+AndreLukyanov@users.noreply.github.com>
2019-11-29 19:21:57 +0300

    addd

---
+++ analysis/js/slider.js
@@ -0,0 +1,12 @@
+let next = document.getElementById("next");
+let items = document.getElementById("items");
+// let img = document.getElementById("sss");
+
+document.querySelector(".slider__item >img").src ="img/" + images[0];
+
+next.onclick = function slider() {
+
+    documentimg.classList.add("slider__images--active");
+}
+
+
