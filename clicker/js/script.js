658ac50f5ae9893cdff8b7daac5f656bac96d4f3
AndreLukyanov <Lukyanov-Andrey-V@yandex.ru>
2020-01-15 23:01:12 +0300

    Обновление 0.4

--- clicker/js/script.js
+++ clicker/js/script.js
@@ -19,190 +19,6 @@ function openPopup() {
 
 openPopup();
 
-
-// Выбор героя //
-
-// let hero = document.querySelectorAll(".choice-character__box .choice-character__item img");
-let character = document.querySelector(".character .character__body img");
-let level = document.querySelector(".character .character__body .character__level .character__scale");
-let nameHero = document.querySelector(".character .character__body .character__name");
-
-let heroes = [
-	{
-		id: 0,
-		hp: 587,
-		name: "anti-mage",
-		url: "/img/character/anti-mage.png"
-	},
-	{
-		id: 1,
-		hp: 587,
-		name: "tusk",
-		url: "/img/character/tusk-full.png"
-	},
-	{
-		id: 2,
-		hp: 625,
-		name: "axe",
-		url: "/img/character/axe-full.png"
-	},
-	{
-		id: 3,
-		hp: 530,
-		name: "juggernaut",
-		url: "/img/character/juggernaut-full.png"
-	},
-	{
-		id: 4,
-		hp: 549,
-		name: "phantom-assassin",
-		url: "/img/character/phantom_assassin-full.png"
-	},
-	{
-		id: 5,
-		hp: 606,
-		name: "ursa",
-		url: "/img/character/ursa-full.png"
-	}
-];
-
-// Создание div и img
-
-function addCharacterInPopup() {
-	for (let i = 1; i <= heroes.length; i++) {
-
-		let characterBoxItem = document.createElement("div");
-		let imagesCharacter = document.createElement("img");
-
-		characterBoxItem.className = "choice-character__item";
-		document.querySelector(".choice-character__box").appendChild(characterBoxItem);
-
-		Array.prototype.forEach.call(document.querySelectorAll(".choice-character__item"), function (e) {
-			e.appendChild(imagesCharacter);
-			imagesCharacter.className = "choice-character__images";
-		});
-	}
-}
-
-addCharacterInPopup();
-
-function addHero() {
-
-	// блок где лежит изображение //
-	let characterItem = Array.from(document.querySelectorAll(".choice-character__item"));
-
-	// изображения //
-	let characterAvatar = Array.from(document.querySelectorAll(".choice-character__images"));
-
-	heroes.forEach(function (item, i) {
-
-		characterItem.forEach(function (elem, index) {
-			
-			if (i == 0) {
-				characterAvatar[0].src = item.url;
-			}
-			else if (i == 1) {
-				characterAvatar[1].src = item.url;
-			}
-			else if (i == 2) {
-				characterAvatar[2].src = item.url;
-			}
-			else if (i == 3) {
-				characterAvatar[3].src = item.url;
-			}
-			else if (i == 4) {
-				characterAvatar[4].src = item.url;
-			}
-			else if (i == 5) {
-				characterAvatar[5].src = item.url;
-			}
-
-
-			function addingHero() {
-
-				elem.onclick = function () {
-					popup.classList.remove("active"); // Удаление класса у popup, чтобы закрыть окно
-
-					if (index == 0) {
-						character.src = characterAvatar[0].src;
-						nameHero.innerHTML = heroes[0].name;
-						level.innerHTML = heroes[0].hp;
-					}
-					else if (index == 1) {
-						character.src = characterAvatar[1].src;
-						nameHero.innerHTML = heroes[1].name;
-						level.innerHTML = heroes[1].hp;
-					}
-					else if (index == 2) {
-						character.src = characterAvatar[2].src;
-						nameHero.innerHTML = heroes[2].name;
-						level.innerHTML = heroes[2].hp;
-					}
-					else if (index == 3) {
-						character.src = characterAvatar[3].src;
-						nameHero.innerHTML = heroes[3].name;
-						level.innerHTML = heroes[3].hp;
-					}
-					else if (index == 4) {
-						character.src = characterAvatar[4].src;
-						nameHero.innerHTML = heroes[4].name;
-						level.innerHTML = heroes[4].hp;
-					}
-					else if (index == 5) {
-						character.src = characterAvatar[5].src;
-						nameHero.innerHTML = heroes[5].name;
-						level.innerHTML = heroes[5].hp;
-					}
-
-				}
-
-			}
-
-			addingHero();
-
-		});
-
-	});
-}
-
-addHero();
-
-// Открытие ЛАВКИ //
-
-function visible() {
-	let armor = document.getElementById("armor");
-
-	armor.classList.toggle("armor-visible");
-}
-
-
-// Получение придмета при клике //
-
-function addItems() {
-	let items = Array.from(document.querySelectorAll(".armor .armor__item img")); // Лавка
-	let inventory = Array.from(document.querySelectorAll(".items .items__col img")); // Инвентарь
-
-	items.forEach(function (item, i) {
-		item.onclick = function () {
-			console.log(i);
-
-			inventory.filter(function (e) {
-				if (inventory.indexOf(e) == 0) {
-					e.src = item.src;
-				}
-				else {
-					e.src = "";
-				}
-			});
-
-		}
-	});
-
-}
-
-addItems();
-
-
 // Добавление coins //
 
 let coin = document.getElementById("coin");
@@ -217,16 +33,14 @@ function addCoins() {
 
 }
 
-// addCoins(); перенес в "Старт игры" для запуска скрипта после нажатия на кнопку начать
-
-
 // Старт игры //
 
-let buttonStart = document.getElementById("button-start");
-let creeps = document.getElementById("creeps");
-let sckillBlock = document.getElementById("skill");
-
 function gameStart() {
+
+	let buttonStart = document.getElementById("button-start");
+	let creeps = document.getElementById("creeps");
+	let sckillBlock = document.getElementById("skill");
+
 	editPopup.style.display = "none";
 	buttonStart.style.display = "none";
 	addCoins();
@@ -353,6 +167,166 @@ function useSkills() {
 }
 
 useSkills();
+
+// Открытие ЛАВКИ //
+
+function visible() {
+	let armor = document.getElementById("armor");
+
+	armor.classList.toggle("armor-visible");
+}
+
+
+// Получение придмета при клике //
+
+function addItems() {
+	let items = Array.from(document.querySelectorAll(".armor .armor__item img")); // Лавка
+	let inventory = Array.from(document.querySelectorAll(".items .items__col img")); // Инвентарь
+
+	items.forEach(function (item, i) {
+		item.onclick = function () {
+			console.log(i);
+
+			inventory.filter(function (e) {
+				if (inventory.indexOf(e) == 0) {
+					e.src = item.src;
+				}
+				else {
+					e.src = "";
+				}
+			});
+
+		}
+	});
+
+}
+
+addItems();
+
+
+// Выбор героя //
+
+// let hero = document.querySelectorAll(".choice-character__box .choice-character__item img");
+let character = document.querySelector(".character .character__body img");
+let characterHealth = document.querySelector(".character .character__body .character__level .character__scale");
+let characterName = document.querySelector(".character .character__body .character__name");
+
+let heroes = [
+	
+		{
+			id: 0,
+			hp: 587,
+			name: "anti-mage",
+			url: "/img/character/anti-mage.png"
+		},
+		{
+			id: 1,
+			hp: 587,
+			name: "tusk",
+			url: "/img/character/tusk-full.png"
+		},
+		{
+			id: 2,
+			hp: 625,
+			name: "axe",
+			url: "/img/character/axe-full.png"
+		},
+		{
+			id: 3,
+			hp: 530,
+			name: "juggernaut",
+			url: "/img/character/juggernaut-full.png"
+		},
+		{
+			id: 4,
+			hp: 549,
+			name: "phantom-assassin",
+			url: "/img/character/phantom_assassin-full.png"
+		},
+		{
+			id: 5,
+			hp: 606,
+			name: "ursa",
+			url: "/img/character/ursa-full.png"
+		}
+	
+];
+
+// Создание div и img
+
+function addCharacterInPopup() {
+	
+	for (let i = 0; i <= heroes.length; i++) {
+
+		let characterBoxItem = document.createElement("div");
+		let imagesCharacter = document.createElement("img");
+
+		characterBoxItem.className = "choice-character__item";
+		document.querySelector(".choice-character__box").appendChild(characterBoxItem);
+
+		Array.prototype.forEach.call(document.querySelectorAll(".choice-character__item"), function (e) {
+			e.appendChild(imagesCharacter);
+			imagesCharacter.className = "choice-character__images";
+		});
+
+		// Добавление пути к img
+
+		imagesCharacter.src = heroes[i].url;
+
+		// Добавление персонажа по клику
+
+		characterBoxItem.onclick = () => {
+			popup.classList.remove("active"); // Удаление класса у popup, чтобы закрыть окно
+
+			character.src = heroes[i].url;
+			characterName.innerHTML = heroes[i].name;
+			characterHealth.innerHTML = heroes[i].hp;
+		};
+
+	};
+};
+
+addCharacterInPopup();
+
+
+// Открытие ЛАВКИ //
+
+function visible() {
+	let armor = document.getElementById("armor");
+
+	armor.classList.toggle("armor-visible");
+}
+
+
+// Получение придмета при клике //
+
+function addItems() {
+	let items = Array.from(document.querySelectorAll(".armor .armor__item img")); // Лавка
+	let inventory = Array.from(document.querySelectorAll(".items .items__col img")); // Инвентарь
+
+	items.forEach(function (item, i) {
+		item.onclick = function () {
+			console.log(i);
+
+			inventory.filter(function (e) {
+				if (inventory.indexOf(e) == 0) {
+					e.src = item.src;
+				}
+				else {
+					e.src = "";
+				}
+			});
+
+		}
+	});
+
+}
+
+addItems();
+
+
+// addCoins(); перенес в "Старт игры" для запуска скрипта после нажатия на кнопку начать
+
 
 
 
