3b9e5598f8e1169ecba79ce695c215b4263ed095
AndreLukyanov <44945472+AndreLukyanov@users.noreply.github.com>
2020-01-23 19:38:20 +0300

    Обновление 0.8

--- clicker/js/gettting-item.js
+++ clicker/js/gettting-item.js
@@ -1,6 +1,7 @@
 function getItems() {
 	let shop = Array.from(document.querySelectorAll(".armor .armor__item img")); // Лавка
 	let inventory = Array.from(document.querySelectorAll(".items .items__col img")); // Инвентарь
+	let inventoryBox = Array.from(document.querySelectorAll(".interface .items")); // Коробка инвентаря
 	let inventaryIndex=0;
 	shop.forEach(function (item, ii) {
 		item.onclick = function () {
@@ -13,6 +14,20 @@ function getItems() {
 				inventaryIndex = 0; // Доходит до 6 и возращается к 0
 			}
 		}
+
+		function dragInventory() {
+			
+			inventoryBox.forEach(function (inv) {
+				item.style.left = inv.offsetHeight / 2 + 'px';
+				item.style.left = inv.offsetWidth / 2 + 'px';
+			});
+	
+		}
+
+		inventory.onmousemove  = function(e) {
+			dragInventory(e);
+		}
+
 	});
 
 }
